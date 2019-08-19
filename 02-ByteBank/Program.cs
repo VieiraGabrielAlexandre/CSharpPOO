1b0aa599acdcb6843ff4d54c01be684f3c7db2d5
Gabriel <gabrielvieira840@gmail.com>
2019-08-19 16:06:20 -0300

    POO

---
+++ 02-ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            ContaCorrente contaDoGabriel = new ContaCorrente();
+            contaDoGabriel.titular = Console.ReadLine();
+            Console.WriteLine(contaDoGabriel.titular);
+            Console.WriteLine(contaDoGabriel.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoGabriel = new ContaCorrente ();
+            contaDoGabriel.titular = Console.ReadLine();
+            contaDoGabriel.agencia = 863;
+            contaDoGabriel.numero = 11;
+
+            ContaCorrente contaVinculada = new ContaCorrente ();
+            contaVinculada.titular = Console.ReadLine();
+            contaVinculada.agencia = 863;
+            contaVinculada.numero = 11;
+
+            Console.WriteLine(contaDoGabriel == contaVinculada);
+
+            contaVinculada = contaDoGabriel;
+            Console.WriteLine(contaDoGabriel == contaVinculada);
+
+            contaDoGabriel.saldo = 1000;
+
+            Console.WriteLine(contaVinculada.saldo);
+            Console.ReadLine();
+        }
+    }
+}
