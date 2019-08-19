37710164afbaa8ba8742ec6801401c3c8ebb995a
Gabriel <gabrielvieira840@gmail.com>
2019-08-19 11:11:03 -0300

    Start

---
+++ ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string titular = "Gabriel";
+            int numeroAgencia = 863;
+            int numeroConta = 151797;
+            int saldo = 100;
+        }
+    }
+}
