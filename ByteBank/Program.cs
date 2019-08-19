5b35c1e43729ab4218d9a2e7bdd1e6d297387e89
Gabriel <gabrielvieira840@gmail.com>
2019-08-19 11:52:14 -0300

    Sistema Banco inicializado

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,10 +10,42 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            string titular = "Gabriel";
-            int numeroAgencia = 863;
-            int numeroConta = 151797;
-            int saldo = 100;
+            ContaCorrente contaDoGabriel = new ContaCorrente();
+
+            Console.WriteLine("Digite o titular: ");
+            contaDoGabriel.titular = Console.ReadLine();
+
+            Console.WriteLine("Digite o numero da conta: ");
+            contaDoGabriel.numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o numero da agencia: ");
+            contaDoGabriel.agencia = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o Saldo: ");
+            contaDoGabriel.saldo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Titular = " + contaDoGabriel.titular);
+            Console.WriteLine("Numero = " + contaDoGabriel.numero);
+            Console.WriteLine("Agencia = " + contaDoGabriel.agencia);
+            Console.WriteLine("Saldo = " + contaDoGabriel.saldo);
+
+            Console.WriteLine("Deseja acrescentar saldo ? 1(SIM)/2(NÃO)");
+
+            int opcao = Convert.ToInt32(Console.ReadLine());
+
+            switch(opcao)
+            {
+                case 1:
+                    Console.WriteLine("Digite o valor a acrescentar: ");
+                    int acrescimo = Convert.ToInt32(Console.ReadLine());
+                    contaDoGabriel.saldo += acrescimo;
+                    Console.WriteLine("O valor atualizado é = " + contaDoGabriel.saldo);
+                    break;
+                case 2:
+                    Console.WriteLine("Finalizando ...");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
